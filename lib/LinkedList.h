e30317be14bfad90decd86d22add313c0c46976d
Mr. Carrot <177513746+Mr-Carrot0@users.noreply.github.com>
2026-01-09 17:13:28 +0100

    Update LinkedList.h

--- lib/LinkedList.h
+++ lib/LinkedList.h
@@ -1,18 +1,8 @@
 #pragma once
+#pragma once
 #include <iostream>
 #include <functional>
 #include <cassert>
-
-#ifndef EXLUDE_RECURSIVE
-
-#ifndef RECURSIFY_FUNCTION
-#define RECURSIFY_FUNCTION(FN, TYPE) 	template<typename ... _Types> \
-TYPE FN(T first, _Types... rest) { \
-	FN(first); \
-	FN(rest...); \
-};
-#endif  !RECURSIFY_FUNCTION
-#endif !EXLUDE_RECURSIVE
 
 // base element class for LinkedList
 template <typename T>
@@ -26,23 +16,12 @@ struct Node {
 template <typename T>
 struct LinkedList {
 	Node<T>* head;
-	/*only here to make push_back faster, so if it becomes a bottlenack, remove it*/
 	Node<T>* tail;
 
 	LinkedList() {
 		head = tail = nullptr;
 	}
 
-	template<typename ... _Types>
-	LinkedList(_Types... items) {
-		head = tail = nullptr;
-		push_back(items...);
-	}
-
-	/*
-	* if multiple agruments are passed, they are proccessed in reverse
-	* 
-	*/
 	void push_front(T item) {
 		Node<T>* new_beg = new Node<T>(item);
 
@@ -58,20 +37,35 @@ struct LinkedList {
 		if (this->_init_if_no_head(new_end)) {
 			/*head = tail = new_end;
 			return;*/
-		tail->next = new_end;
-		tail = new_end;
+			tail->next = new_end;
+			tail = new_end;
 		}
 	}
-	#ifndef EXLUDE_RECURSIVE
-	RECURSIFY_FUNCTION(push_front, void)
-	RECURSIFY_FUNCTION(push_back, void)
-	#endif !EXLUDE_RECURSIVE
+#ifndef EXLUDE_RECURSIVE
+	template<typename ... _Types>
+	void push_front(T first, _Types... rest) {
+
+		push_front(rest...);
+		push_front(first);
+	};
+	template<typename ... _Types>
+	void push_back(T first, _Types... rest) {
+
+		push_back(first);
+		push_back(rest...);
+	};
+	template<typename ... _Types>
+	LinkedList(_Types... items) {
+		head = tail = nullptr;
+		push_front(items...);
+	}
+#endif !EXLUDE_RECURSIVE
 
 	T operator[](unsigned int index) {
 		unsigned int count = 0;
 		Node<T>* curr = head;
 		do {
-			if (count == index) 
+			if (count == index)
 				return curr->data;
 			//std::cout << curr->data;
 			curr = curr->next;
@@ -83,23 +77,23 @@ struct LinkedList {
 		//throw std::runtime_error("index out of range");
 	}
 
-	T pop(unsigned int index){
-		unsigned int count=0u;
-		Node<T>* curr=head;
-		Node<T>* prev=nullptr;
-		Node<T>* next=nullptr;
+	T pop(unsigned int index) {
+		unsigned int count = 0u;
+		Node<T>* curr = head;
+		Node<T>* prev = nullptr;
+		Node<T>* next = nullptr;
 		T wanted;
 		do {
 			if (count == index - 1) {
 				prev = curr;
 
-				assert((void("index out of range"), curr->next!=nullptr));
+				assert((void("index out of range"), curr->next != nullptr));
 				wanted = curr->next->data;
 				//delete curr->next;
-				if(
+				if (
 					curr->next // index
 					&& curr->next->next // index + 1
-					){
+					) {
 					curr->next = curr->next->next;
 				}
 				return wanted;
@@ -112,16 +106,16 @@ struct LinkedList {
 		assert((void("index out of range"), false));
 	}
 
-	void remove(unsigned int index){
-		unsigned int count=0u;
-		Node<T>* curr=head;
-		Node<T>* prev=nullptr;
+	void remove(unsigned int index) {
+		unsigned int count = 0u;
+		Node<T>* curr = head;
+		Node<T>* prev = nullptr;
 		//Node<T>* next=nullptr;
 		do {
 			if (count == index - 1) {
 				prev = curr;
 				Node<T>* _next = curr->next;
-				
+
 				// if end of array, we can skip this
 				if (_next->next != nullptr)
 				{
@@ -134,7 +128,8 @@ struct LinkedList {
 		} while (curr != nullptr);
 	}
 
-	size_t Length(){
+
+	size_t Length() {
 		if (head == nullptr)
 			return 0;
 		if (head->next == nullptr)
@@ -160,6 +155,15 @@ struct LinkedList {
 		return count;
 	}
 
+	void print() {
+		if (head == nullptr) return;
+
+		Node<T>* curr = head;
+		do {
+			std::cout << curr->data << "\n";
+			curr = curr->next;
+		} while (curr != nullptr);
+	}
 
 	void forEach(std::function<void(Node<T>*)> fn) {
 		Node<T>* curr = head;
@@ -170,7 +174,6 @@ struct LinkedList {
 		} while (curr != nullptr);
 	}
 	void forEach(std::function<void(T)> fn) {
-		if (head == nullptr) return;
 		Node<T>* curr = head;
 
 		do {
@@ -178,19 +181,10 @@ struct LinkedList {
 			curr = curr->next;
 		} while (curr != nullptr);
 	}
-	//void print() {
-	//	if (head == nullptr) return;
-
-	//	Node<T>* curr = head;
-	//	do {
-	//		std::cout << curr->data << "\n";
-	//		curr = curr->next;
-	//	} while (curr != nullptr);
-	//}
 
 private:
 	inline bool _init_if_no_head(Node<T>* ele) {
-		if (head != nullptr) 
+		if (head != nullptr)
 			return true;
 
 		head = tail = ele;
